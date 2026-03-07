2e0ac057a508bc16c8f249a002ee73f40c6de181
examplyz <slubsku2.0@gmail.com>
2026-03-07 10:16:48 +0100

    Error page is added , connecting fonts

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import styles from "./NotFoundPage.module.scss"
+import {NavLink} from "react-router";
+import robot_img from "../../assets/images/robot_img.png"
 
 const NotFoundPage = () => {
+	const [counter, setCounter] = useState(20)
+	useEffect(() => {
+		const interval = setInterval(() => setCounter(counter - 1), 1000)
+		return (() => {
+			clearInterval(interval)
+		})
+	}, [counter])
 	return (
-		<div>
-			Not Found Page
+		<div className={styles.container}>
+			<img src={robot_img} alt="Robot"/>
+			<h2 className={styles.heading}>The page is not found</h2>
+			<p>
+				You will be redirected to the
+				<NavLink to={"/todos"}> main page </NavLink> in {counter} seconds....
+			</p>
+			{/*{!counter && <Navigate to={"/todos"}/>}*/}
 		</div>
 	);
 };
